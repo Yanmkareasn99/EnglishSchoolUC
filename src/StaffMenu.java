8e1f017a120c85b26b79ecc892e468e85cb97006
Yanmkareasn99 <1yanmyokyaw@gmail.com>
2026-01-26 23:40:28 +0900

    added new staff menu

--- src/StaffMenu.java
+++ src/StaffMenu.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class StaffMenu {
 
     public static void showMenu(){
-        System.out.println("""
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            try{
+                System.out.println("""
                 
                 1:登録 
                 2:照会 
@@ -10,6 +14,36 @@ public class StaffMenu {
                 5:予約代行 
                 6:取消 
                 0:戻る""");
+
+                int choice = Integer.parseInt(sc.nextLine());
+                switch (choice){
+                    case 1 -> {
+                        addStudent();
+                    }
+                    case 2 -> {
+                        viewStudents();
+                    }
+                    case 3 -> {
+                        changeStudent();
+                    }
+                    case 4 -> {
+                        addPoints();
+                    }
+                    case 5 -> {
+                        resreveLesson();
+                    }
+                    case 6 -> {
+                        cancelLesson();
+                    }
+                    case 0-> {
+                        return;
+                    }
+                    default -> System.out.println("無効な入力！！！");
+                }
+            } catch (Exception e) {
+                System.out.println("数字を入力してください");
+            }
+        }
 
     }
     public static void viewStudents() {
